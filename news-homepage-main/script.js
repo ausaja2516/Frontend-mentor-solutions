e980ee5514e97e49accb5b5e0add582220ea8efe
ausaja2516 <ausi2516@gmail.com>
2024-05-08 15:16:36 +0500

    fixed mobile navigation

--- news-homepage-main/script.js
+++ news-homepage-main/script.js
@@ -1,10 +1,22 @@
 const menuBtn = document.querySelector(".menu_btn");
 const nav = document.querySelector(".nav_list");
 const menuImg = document.querySelector(".menu_image");
+const overlay = document.querySelector(".overlay");
 
 let change = false;
+
 menuBtn.addEventListener("click", () => {
-  nav.classList.toggle("animate");
-  if (!change) menuImg.setAttribute("src", "./assets/images/icon-menu.svg");
-  else menuImg.setAttribute("src", "./assets/images/icon-menu-close.svg");
+  if (!change) {
+    nav.classList.add("animate");
+    menuImg.setAttribute("src", "./assets/images/icon-menu-close.svg");
+    overlay.classList.add("animate");
+
+    change = true;
+  } else {
+    nav.classList.remove("animate");
+    menuImg.setAttribute("src", "./assets/images/icon-menu.svg");
+    overlay.classList.remove("animate");
+
+    change = false;
+  }
 });
