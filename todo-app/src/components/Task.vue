1a0643218779b793f64bcdbe066be6ccced6e8e2
ausaja2516 <ausi2516@gmail.com>
2024-09-28 04:51:50 +0500

    todo app added

---
+++ todo-app/src/components/Task.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Transition>
+      <li class="flex justify-between items-center p-4">
+        <p>{{ task.title }}</p>
+        <input
+          type="checkbox"
+          v-model="task.isComplete"
+          class="ml-2 cursor-pointer"
+        />
+      </li>
+    </Transition>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  task: {
+    type: Object,
+    default: {},
+  },
+});
+</script>
+<style scoped></style>
---
+++ todo-app/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Tasks from "@/components/Tasks.vue";
+</script>
+
+<template>
+  <main
+    class="grid min-h-screen place-items-center font-mono text-gray-50 bg-slate-950"
+  >
+    <img
+      class="fixed top-0 z-0"
+      src="https://todo-app-fementor-rc60gijyw-quangnphan.vercel.app/images/bg-desktop-dark.jpg"
+      alt=""
+      srcset=""
+    />
+    <Tasks />
+  </main>
+</template>
